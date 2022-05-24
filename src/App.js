3b17b5b6046fbc9c5561f813cd82103a270d31ab
Michael Ackley <michaelack27@gmail.com>
2022-05-24 14:36:53 -0400

    Michael Commit

--- src/App.js
+++ src/App.js
@@ -8,8 +8,30 @@ import NavBar from './components/navbar';
 import Teacher from './components/teacher';
 import Student from './components/student';
 import Calendar from './components/calendar';
+import { initializeApp } from "firebase/app";
+import {collection, doc, updateDoc, addDoc, getDocs, getFirestore} from "firebase/firestore";
+
+//Test Push
 
 function App() {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCJTsf-MF41gqqyZP1ZRP4uG9xkuTh3Wvs",
+    authDomain: "week1-team11.firebaseapp.com",
+    projectId: "week1-team11",
+    storageBucket: "week1-team11.appspot.com",
+    messagingSenderId: "574163223150",
+    appId: "1:574163223150:web:e1d12475d686c0b837dc7f"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const database = getFirestore(app);
+
+  const retreivesentences = () => {
+    getDocs(collection(database, "Class"))
+        .then((allDocs) => {allDocs.forEach((doc) => console.log(doc.data()))})
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -21,6 +43,10 @@ function App() {
           <Route  element={<Calendar/>} path="calendar" />
         </Routes>
       </BrowserRouter>
+
+    <div>
+      {retreivesentences()}
+    </div>
     </div>
   );
 }
