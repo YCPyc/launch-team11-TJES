a0a95ab148c5e588e51f6178b830f697ab3e08d2
jcqb09 <jcqb0909@gmail.com>
2022-05-26 16:26:53 -0400

    prep for merge to main

--- src/App.js
+++ src/App.js
@@ -1,79 +1,67 @@
-import './App.css';
-import { BrowserRouter,
-  Routes,
-  Route, 
-} from 'react-router-dom';
-import Home from './components/home';
-import NavBar from './components/navbar';
-import Teacher from './components/teacher';
-import Student from './components/student';
-import Calendar from './components/calendar';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/home";
+import NavBar from "./components/navbar";
+import Teacher from "./components/teacher";
+import Student from "./components/student";
+import Calendar from "./components/calendar";
 import { initializeApp } from "firebase/app";
-import {collection, doc, updateDoc, addDoc, getDocs, getFirestore} from "firebase/firestore";
-<<<<<<< HEAD
-=======
-import Classtwopage from './components/classtwopage';
-import Classonepage from './components/classonepage';
-import Classthreepage from './components/classthreepage';
-import Classfourpage from './components/classfourpage'
-import useState from 'react';
->>>>>>> 4454f4ef2c438964fed5d5e104d7c70af5077741
-
+import {
+  collection,
+  doc,
+  updateDoc,
+  addDoc,
+  getDocs,
+  getFirestore,
+} from "firebase/firestore";
+import Classtwopage from "./components/classtwopage";
+import Classonepage from "./components/classonepage";
+import Classthreepage from "./components/classthreepage";
+import Classfourpage from "./components/classfourpage";
+import useState from "react";
 
 function App() {
-
   const firebaseConfig = {
-<<<<<<< HEAD
-  apiKey: process.env.REACT_APP_apiKey,
-=======
-   apiKey: process.env.REACT_APP_apiKey,
->>>>>>> 4454f4ef2c438964fed5d5e104d7c70af5077741
-   authDomain: process.env.REACT_APP_authDomain,
-   projectId: process.env.REACT_APP_projectId,
-   storageBucket: process.env.REACT_APP_storageBucket,
-   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-   appId: process.env.REACT_APP_appId
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
   };
 
   const app = initializeApp(firebaseConfig);
   const database = getFirestore(app);
 
   //Michael Practing retreiving everything:
-<<<<<<< HEAD
-=======
-  
->>>>>>> 4454f4ef2c438964fed5d5e104d7c70af5077741
-  /*
-  const retreivesentences = () => {
-    getDocs(collection(database, "Class"))
-        .then((allDocs) => {allDocs.forEach((doc) => console.log(doc.data()))})
-  }
-  */
-<<<<<<< HEAD
-=======
-  
->>>>>>> 4454f4ef2c438964fed5d5e104d7c70af5077741
+
+  // const retreivesentences = () => {
+  //   getDocs(collection(database, "Class"))
+  //       .then((allDocs) => {allDocs.forEach((doc) => console.log(doc.data()))})
+  // }
 
   return (
-
     <div className="App">
       <BrowserRouter>
-        <NavBar/>
+        <NavBar />
         <Routes>
-          <Route  element={<Home/>} path="/" />
-          <Route element={<Teacher/>} path="teacher" />
-          <Route  element={<Student/>} path="student" />
-          <Route  element={<Calendar/>} path="calendar" />
-          <Route element={<Classtwopage db={database}/>} path="classtwopage" />
-          <Route element={<Classonepage db={database}/>} path="classonepage" />
-          <Route element={<Classthreepage db={database}/>} path="classthreepage" />
-          <Route element={<Classfourpage db={database}/>} path='classfourpage'/>
-
+          <Route element={<Home />} path="/" />
+          <Route element={<Teacher />} path="teacher" />
+          <Route element={<Student />} path="student" />
+          <Route element={<Calendar />} path="calendar" />
+          <Route element={<Classtwopage db={database} />} path="classtwopage" />
+          <Route element={<Classonepage db={database} />} path="classonepage" />
+          <Route
+            element={<Classthreepage db={database} />}
+            path="classthreepage"
+          />
+          <Route
+            element={<Classfourpage db={database} />}
+            path="classfourpage"
+          />
         </Routes>
       </BrowserRouter>
-
     </div>
-
   );
 }
 
--- src/components/classonepage.js
+++ src/components/classonepage.js
@@ -1,157 +1,174 @@
-import {collection, doc, updateDoc, addDoc, getDocs, getFirestore, setDoc, deleteDoc} from "firebase/firestore";
-import {useEffect} from "react";
-import {useState, useRef} from "react";
-import Grid from '@mui/material/Grid';
-import { DataGrid } from '@mui/x-data-grid';
-import {TableContainer, Table, TableHead, TableBody,TableRow,TableCell,Paper, Button} from '@mui/material'
+import {
+  collection,
+  doc,
+  updateDoc,
+  addDoc,
+  getDocs,
+  getFirestore,
+  setDoc,
+  deleteDoc,
+} from "firebase/firestore";
+import { useEffect } from "react";
+import { useState, useRef } from "react";
+import Grid from "@mui/material/Grid";
+// import { DataGrid } from '@mui/x-data-grid';
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Paper,
+  Button,
+} from "@mui/material";
 
+function Classonepage({ db }) {
+  const [responses, setResponses] = useState([]);
+  const [classArray, setClassArray] = useState();
+  const [studentsArray, setStudentsArray] = useState([]);
+  const [secondArray, setSecondArray] = useState([]);
+  const [listOfStudents, setListOfStudents] = useState("");
+  const [arrayOfStudents, setArrayOfStudents] = useState([]);
+  const [otherList, setOtherList] = useState("");
+  const textFieldRef = useRef(null);
+  const [nameString, setnameString] = "Class2Student";
+  const [finalList, setFinalList] = [];
+  const otherRef = useRef();
+  const editRefOne = useRef();
+  const editRefTwo = useRef();
+  const editRefThree = useRef();
+  const [editBox, setEditBox] = useState(false);
+  let studentnumber = 7;
+  //let nameString = "Class2Student";
 
+  useEffect(() => {
+    const responses = [];
+    getDocs(collection(db, "Class")) // get the collection
+      .then((allResponses) => {
+        // format each response into an array as we want it
+        allResponses.forEach((response) =>
+          responses.push({ id: response.id, ...response.data() })
+        );
+        setResponses(responses);
+      });
+    getStudents();
+  }, [db]);
 
-function Classonepage({db}){
+  const getStudents = () => {
+    const rsp = [];
+    const nms = [];
+    const grades = [];
+    getDocs(collection(db, "Class/TestClass1/TC1Students")) // get the collection
+      .then((allResponses) => {
+        // format each response into an array as we want it
+        allResponses.forEach((response) =>
+          rsp.push({ id: response.id, ...response.data() })
+        );
+        //console.log(rsp)
+        setStudentsArray(rsp);
+        rsp.forEach((i) => nms.push(i.id, i.grade));
+        //rsp.forEach((i) => nms.push(i.grade))
+        console.log(nms);
+        //setClassArray(rsp[0])
+        //delete rsp[0].id
+        //console.log(Object.values(rsp[0]))
+        setListOfStudents(Object.values(nms).join(", "));
+        //setOtherList();
+      });
+  };
 
-    const [responses, setResponses] = useState([]);
-    const [classArray, setClassArray] = useState();
-    const [studentsArray, setStudentsArray] = useState([]);
-    const [secondArray, setSecondArray] = useState([]);
-    const [listOfStudents, setListOfStudents] = useState("");
-    const [arrayOfStudents, setArrayOfStudents] = useState([])
-    const [otherList, setOtherList] = useState("");
-    const textFieldRef = useRef(null);
-    const [nameString, setnameString] = ("Class2Student")
-    const [finalList, setFinalList] = ([])
-    const otherRef = useRef();
-    const editRefOne = useRef();
-    const editRefTwo = useRef();
-    const editRefThree = useRef();
-    const [editBox, setEditBox] = useState(false);
-    let studentnumber = 7;
-    //let nameString = "Class2Student";
+  function getid(item) {
+    const tlist = [];
+    console.log(item.id);
 
-    useEffect(() => {
-        const responses = []
-        getDocs(collection(db, "Class"))  // get the collection
-        .then((allResponses) => {  // format each response into an array as we want it
-          allResponses.forEach((response) => responses.push({ id: response.id, ...response.data() }))
-          setResponses(responses)
-        })
-        getStudents();
-      }, [db])
-    
+    return;
+  }
 
+  const addResponse = (e) => {
+    e.preventDefault(); // no reloading the page
+    console.log("text field ref: ", otherRef.current.value);
 
-    const getStudents = () => {
-        const rsp = []
-        const nms = []
-        const grades = []
-        getDocs(collection(db, "Class/TestClass1/TC1Students"))  // get the collection
-        .then((allResponses) => {  // format each response into an array as we want it
-          allResponses.forEach((response) => rsp.push({ id: response.id, ...response.data() }))
-          //console.log(rsp)
-          setStudentsArray(rsp)
-          rsp.forEach((i) => nms.push(i.id,i.grade))
-          //rsp.forEach((i) => nms.push(i.grade))
-          console.log(nms)
-          //setClassArray(rsp[0])
-          //delete rsp[0].id
-          //console.log(Object.values(rsp[0]))
-          setListOfStudents((Object.values(nms)).join(', '));
-          //setOtherList();
-        })
-    }
+    //const newResponse = textFieldRef.current.value
 
+    const data = {};
+    const ResponseName = {
+      //nameString: textFieldRef.current.value
+    };
 
-    function getid(item){
-        const tlist = []
-        console.log(item.id)
-        
-        return
+    setDoc(
+      doc(db, "Class/TestClass1/TC1Students", otherRef.current.value),
+      data
+    ); // add the new response
 
-    }
+    //addDoc((doc(db,"Class/Class2/Class2Students", textFieldRef.current.value)))
 
-    const addResponse = (e) => {
-        e.preventDefault();  // no reloading the page
-        console.log("text field ref: ",otherRef.current.value)
-    
+    getStudents();
 
-        //const newResponse = textFieldRef.current.value
-        
-        const data = {
-            
-        };
-        const ResponseName= {
-          //nameString: textFieldRef.current.value
-        }
-        
-        setDoc(doc(db, "Class/TestClass1/TC1Students", otherRef.current.value),data) // add the new response 
+    otherRef.current.value = ""; // clear the text field
+  };
 
-        //addDoc((doc(db,"Class/Class2/Class2Students", textFieldRef.current.value)))
-      
-        getStudents();
-    
-        otherRef.current.value = "" // clear the text field
+  const deleteResponse = (e) => {
+    e.preventDefault();
+
+    //console.log("running delete response")
+    //console.log(textFieldRef.current.value)
+
+    deleteDoc(
+      doc(db, "Class/TestClass1/TC1Students", textFieldRef.current.value)
+    );
+
+    getStudents();
+
+    textFieldRef.current.value = "";
+  };
+
+  const tableDeleteResponse = (input) => {
+    //e.preventDefault();
+
+    //console.log("running delete response")
+    //console.log(textFieldRef.current.value)
+
+    deleteDoc(doc(db, "Class/TestClass1/TC1Students", input));
+
+    getStudents();
+
+    //textFieldRef.current.value = ""
+  };
+
+  const editGrade = (e) => {
+    e.preventDefault();
+
+    updateDoc(
+      doc(db, "Class/TestClass1/TC1Students", editRefOne.current.value),
+      {
+        grade: editRefTwo.current.value,
       }
+    );
 
-    const deleteResponse = (e) => {
-      e.preventDefault();
+    getStudents();
 
-      //console.log("running delete response")
-      //console.log(textFieldRef.current.value)
-      
-      deleteDoc(doc(db,"Class/TestClass1/TC1Students", textFieldRef.current.value))
+    editRefTwo.current.value = "";
+    editRefOne.current.value = "";
+  };
 
+  const tableEditGrade = (input1) => {
+    //e.preventDefault();
 
-      getStudents();
+    console.log(input1);
+    console.log(editRefThree.current.value);
+    updateDoc(doc(db, "Class/TestClass1/TC1Students", input1), {
+      grade: editRefThree.current.value,
+    });
 
-      textFieldRef.current.value = ""
+    getStudents();
 
-    }
+    editRefThree.current.value = "";
+    //editRefTwo.current.value = ""
+    //editRefOne.current.value = ""
+  };
 
-    const tableDeleteResponse = (input) => {
-      //e.preventDefault();
-
-      //console.log("running delete response")
-      //console.log(textFieldRef.current.value)
-      
-      deleteDoc(doc(db,"Class/TestClass1/TC1Students", input))
-
-
-      getStudents();
-
-      //textFieldRef.current.value = ""
-
-    }
-
-    const editGrade = (e) => {
-      e.preventDefault();
-
-      updateDoc(doc(db,"Class/TestClass1/TC1Students", editRefOne.current.value),{
-        grade: editRefTwo.current.value
-      });
-
-      getStudents();
-
-      editRefTwo.current.value = ""
-      editRefOne.current.value = ""
-    }
-
-    const tableEditGrade = (input1) => {
-      //e.preventDefault();
-
-      console.log(input1)
-      console.log(editRefThree.current.value)
-      updateDoc(doc(db,"Class/TestClass1/TC1Students", input1),{
-        grade: editRefThree.current.value
-      });
-
-      getStudents();
-
-      editRefThree.current.value = ""
-      //editRefTwo.current.value = ""
-      //editRefOne.current.value = ""
-    }
-
-
-    /*
+  /*
     <div>
               <form onSubmit={editGrade}>
               <h2>Edit Student Grade:</h2>
@@ -161,83 +178,83 @@ function Classonepage({db}){
               </form>
     </div>
     */
-   
-    
 
-    if (responses.length>0 && studentsArray.length>=0)return(
+  if (responses.length > 0 && studentsArray.length >= 0)
+    return (
+      <div>
         <div>
-            <div>
-            <h1>Class page</h1>
-            <h2>Class: {responses[3].ClassName}</h2>
-            <h2>Subject: {responses[3].Subject}</h2>
-            <h2>Teacher: {responses[3].Teacher._key.path.segments[6]}</h2>
-           
-            <h3></h3>
-        
-            </div>
-            <p></p>
-            
-            <div>
-            
-            </div>
+          <h1>Class page</h1>
+          <h2>Class: {responses[3].ClassName}</h2>
+          <h2>Subject: {responses[3].Subject}</h2>
+          <h2>Teacher: {responses[3].Teacher._key.path.segments[6]}</h2>
 
-
-
-    
-            <div>
-                <h2>Add Student:</h2>
-                <form onSubmit={addResponse}>
-                <input type="text" ref={otherRef} />
-                <input type="Submit" />
-                </form>
-
-                
-            </div>
-
-            <div>
-              <h1>Class Roster:</h1>
-              <TableContainer component={Paper}>
-                <Table aria-label='Roster'>
-                  <TableHead>
-                    <TableRow>
-                      
-                      <TableCell>Name</TableCell>
-                      <TableCell>Grade</TableCell>
-                      <TableCell></TableCell>
-                      </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {studentsArray.map(row => (
-                      <TableRow key={row.id}>
-                        
-                        <TableCell>{row.id}</TableCell>
-                        <TableCell>{row.grade}</TableCell>
-                        <TableCell>  
-                          
-                        </TableCell>
-                        <TableCell><Button variant="contained" onClick={() =>tableDeleteResponse(row.id)}>Delete</Button></TableCell>
-                        
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-              <Button variant="contained" onClick={() =>setEditBox(editBox => !editBox)}>Edit Grades</Button>
-            </div>
-            
-            <div>
-              {editBox==true && 
-              <form onSubmit={editGrade}>
-              <label>Name: <input type="text" ref={editRefOne} /> </label>
-              <label>Grade: <input type="text" ref={editRefTwo} /> </label> 
-              <input type="Submit" />
-              </form>
-            }
-            </div>
-            
+          <h3></h3>
         </div>
-    )
+        <p></p>
 
+        <div></div>
+
+        <div>
+          <h2>Add Student:</h2>
+          <form onSubmit={addResponse}>
+            <input type="text" ref={otherRef} />
+            <input type="Submit" />
+          </form>
+        </div>
+
+        <div>
+          <h1>Class Roster:</h1>
+          <TableContainer component={Paper}>
+            <Table aria-label="Roster">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Grade</TableCell>
+                  <TableCell></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {studentsArray.map((row) => (
+                  <TableRow key={row.id}>
+                    <TableCell>{row.id}</TableCell>
+                    <TableCell>{row.grade}</TableCell>
+                    <TableCell></TableCell>
+                    <TableCell>
+                      <Button
+                        variant="contained"
+                        onClick={() => tableDeleteResponse(row.id)}
+                      >
+                        Delete
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <Button
+            variant="contained"
+            onClick={() => setEditBox((editBox) => !editBox)}
+          >
+            Edit Grades
+          </Button>
+        </div>
+
+        <div>
+          {editBox == true && (
+            <form onSubmit={editGrade}>
+              <label>
+                Name: <input type="text" ref={editRefOne} />{" "}
+              </label>
+              <label>
+                Grade: <input type="text" ref={editRefTwo} />{" "}
+              </label>
+              <input type="Submit" />
+            </form>
+          )}
+        </div>
+      </div>
+    );
 }
 
-export default Classonepage
+export default Classonepage;
